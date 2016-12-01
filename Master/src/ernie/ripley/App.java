b218dea92f580f089b846298aee90f874a628c57
Ernie Ripley <efripley@gmail.com>
2016-12-01 12:14:30 -0700

    Initial committerminal shows

--- Master/src/ernie/ripley/App.java
+++ Master/src/ernie/ripley/App.java
@@ -1,8 +1,18 @@
 package ernie.ripley;
 
 import static kiss.API.*;
+import java.io.*;
+import com.googlecode.lanterna.*;
+import com.googlecode.lanterna.terminal.*;
 
 public class App{
-	void run(){
+
+  Terminal terminal;
+
+	void run() throws IOException {
+    terminal = TerminalFacade.createTerminal();
+    terminal.enterPrivateMode();
+    pause(5);
+    terminal.exitPrivateMode();
 	}
 }
